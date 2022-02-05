1a3824abefbb5363cd7eeb5dd490d1ded2761474
Medhansh100 <84464193+Medhansh100@users.noreply.github.com>
2022-02-05 15:26:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var cat = 0;
+var dog = 0;
+var lion = 0;
+var cow = 0;
+var background_noise = 0;
+
+function startClassifictaion() {
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/Fp_bZ7NqI/',modelReady);
+}
+function modelReady() {
+    classifier.classify(gotResult);
+}
+function gotResult(error, results) {
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
